86bf0fb7f431550aa7624234d28a620162d1be21
neamahmami <79372921+neamahmami@users.noreply.github.com>
2023-12-05 14:57:02 +0100

    commit empty

---
+++ src/test/java/be/heh/projetbook/ProjetBookApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.projetbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
