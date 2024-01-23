6d8829774fe39b91878f9f4d4ed074af6afb65e5
fabiola <fabiola.yepgwatakedo@std.heh.be>
2024-01-23 19:56:35 +0100

    Searchbook dernière version

--- src/main/java/be/heh/projetBook/ProjectConfig.java
+++ src/main/java/be/heh/projetBook/ProjectConfig.java
@@ -1,8 +1,11 @@
 package be.heh.projetBook;
 
 import be.heh.projetBook.adapter.out.persistence.CreateBookRepository;
+import be.heh.projetBook.adapter.out.persistence.SearchBookRepository;
 import be.heh.projetBook.application.domain.service.CreateBookImplementation;
+import be.heh.projetBook.application.domain.service.SearchBookImplementation;
 import be.heh.projetBook.application.port.in.CreateBookUseCase;
+import be.heh.projetBook.application.port.in.SearchBookUseCase;
 import be.heh.projetBook.application.port.out.AddNewBook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,16 +20,21 @@ import org.springframework.web.filter.CorsFilter;
 public class ProjectConfig {
     //@Autowired
     private JdbcTemplate jdbcTemplate;
+    private RestTemplate restTemplate;
 
     @Bean
     CreateBookUseCase getCreateBookUseCase(){
         return new CreateBookImplementation(new CreateBookRepository(jdbcTemplate));
     }
+    @Bean
+    SearchBookUseCase getSearchBookUsecase(){
+        return new SearchBookImplementation(new SearchBookRepository(restTemplate,jdbcTemplate));
+    }
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:3001");
         config.addAllowedHeader("*");
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
@@ -42,5 +50,6 @@ public class ProjectConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 
 }
--- src/main/java/be/heh/projetBook/adapter/out/persistence/SearchBookRepository.java
+++ src/main/java/be/heh/projetBook/adapter/out/persistence/SearchBookRepository.java
@@ -1,10 +1,12 @@
 package be.heh.projetBook.adapter.out.persistence;
 import be.heh.projetBook.adapter.apiResources.ApiDocs;
 import be.heh.projetBook.adapter.apiResources.ApiResponse;
+import be.heh.projetBook.adapter.apiResources.BookInfoRowMapper;
 import be.heh.projetBook.application.domain.model.BookInfo;
 import be.heh.projetBook.application.port.out.ApiRequester;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.*;
 
@@ -16,14 +18,18 @@ public class SearchBookRepository implements ApiRequester {
 
 
     private final RestTemplate restTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    public SearchBookRepository(RestTemplate restTemplate) {
+    public SearchBookRepository(RestTemplate restTemplate, JdbcTemplate jdbcTemplate) {
         this.restTemplate = restTemplate;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
 
     @Override
     public List<BookInfo> RequestBookInfos(String title){
+        String sql = "SELECT * FROM booklist WHERE title = ?";
+        List<BookInfo> bookListInfo = jdbcTemplate.query(sql, new Object[]{title}, new BookInfoRowMapper());
         String bookName = title.replace(' ','+');
         String origin = "https://openlibrary.org/";
         String url = origin+"/search.json?q="+bookName;
@@ -32,49 +38,54 @@ public class SearchBookRepository implements ApiRequester {
         
         List<ApiDocs> docs = response.getDocs();
         List<BookInfo> maliste = new ArrayList<>();
-        if (response != null && docs!= null && !docs.isEmpty()) {
+        if(!bookListInfo.isEmpty()){
+            maliste.addAll(bookListInfo);
+        }
+        else{
+            if (response != null && docs!= null && !docs.isEmpty()) {
 
-            for (ApiDocs doc : docs) {
-                if (doc.getTitle().toUpperCase().contains(title.toUpperCase())) {
-                    BookInfo book = new BookInfo("test", "test", "test", "test", "1234", 0);
-                    String key = doc.getKey();
-                    String urlDescription = origin + key + ".json";
-                    JsonNode responseDescription = restTemplate.getForObject(urlDescription, JsonNode.class);
-                    book.setIsbn(doc.getKey());
-                    String authors = "";
-                    if (doc.getAuthorName() != null) {
-                        for (String author : doc.getAuthorName()) {
-                            authors = author + ',';
-                        }
-                    } else {
-                        authors = null;
-                    }
-                    String isbn = "";
-                    if (doc.getIsbn() != null) {
-                        for (String id : doc.getIsbn()) {
-                            isbn = id + ',';
-                        }
-                    } else {
-                        isbn = null;
-                    }
-                    book.setTitle(doc.getTitle());
-                    book.setAuthor(authors);
-                    book.setIsbn(urlDescription);
-                    book.setCover(doc.getCoverI() != null ? "https://covers.openlibrary.org/b/id/" + doc.getCoverI() + "-L.jpg" : null);
-
-                    if (responseDescription == null || responseDescription.get("description") == null) {
-                        book.setSummary(null);
-                    } else {
-                        String res = responseDescription.get("description").toString();
-
-                        if (res.startsWith("{")) {
-                            String resume = responseDescription.get("description").get("value").asText();
-                            book.setSummary(resume);
+                for (ApiDocs doc : docs) {
+                    if (doc.getTitle().toUpperCase().contains(title.toUpperCase())) {
+                        BookInfo book = new BookInfo("test", "test", "test", "test", "1234", 0);
+                        String key = doc.getKey();
+                        String urlDescription = origin + key + ".json";
+                        JsonNode responseDescription = restTemplate.getForObject(urlDescription, JsonNode.class);
+                        book.setIsbn(doc.getKey());
+                        String authors = "";
+                        if (doc.getAuthorName() != null) {
+                            for (String author : doc.getAuthorName()) {
+                                authors = author + ',';
+                            }
                         } else {
-                            book.setSummary(res);
+                            authors = null;
                         }
+                        String isbn = "";
+                        if (doc.getIsbn() != null) {
+                            for (String id : doc.getIsbn()) {
+                                isbn = id + ',';
+                            }
+                        } else {
+                            isbn = null;
+                        }
+                        book.setTitle(doc.getTitle());
+                        book.setAuthor(authors);
+                        book.setIsbn(isbn);
+                        book.setCover(doc.getCoverI() != null ? "https://covers.openlibrary.org/b/id/" + doc.getCoverI() + "-L.jpg" : null);
+
+                        if (responseDescription == null || responseDescription.get("description") == null) {
+                            book.setSummary(null);
+                        } else {
+                            String res = responseDescription.get("description").toString();
+
+                            if (res.startsWith("{")) {
+                                String resume = responseDescription.get("description").get("value").asText();
+                                book.setSummary(resume);
+                            } else {
+                                book.setSummary(res);
+                            }
+                        }
+                        maliste.add(book);
                     }
-                    maliste.add(book);
                 }
             }
         }
